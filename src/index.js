707873e6f1d367a0c0b8f0c818025ef0203b28bd
Waseem Ahmed <codewaseem@gmail.com>
2020-05-04 08:34:56 +0530

    complete output management guide on webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles/global.css";
 import logoSvg from "./assets/logos/logo.svg";
 import LivvicFont from "./assets/fonts/Livvic-Regular.ttf";
+import printMe from "./print.js";
 
 console.log("Hello, World");
 console.log("You can import styles");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const CSSLoaders = {
   test: /\.css$/,
@@ -16,11 +18,20 @@ const FontLoaders = {
 };
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   module: {
     rules: [CSSLoaders, ImageFilesLoaders, FontLoaders],
   },
